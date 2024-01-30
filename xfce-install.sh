39f024671f408db51a76cf205f4e73b868217b8e
Giraffes19 <100843724+Giraffes19@users.noreply.github.com>
2024-01-30 00:01:51 +0000

    Update xfce-install.sh
    added neofetch

--- xfce-install.sh
+++ xfce-install.sh
@@ -27,11 +27,12 @@ apt install -y \
     adwaita-qt \
     qt5ct \
     linux-headers-amd64 \ 
-    nvidia-driver firmware-misc-nonfree
-curl -fsSL https://ppa.ablaze.one/KEY.gpg | sudo gpg --dearmor -o /usr/share/keyrings/Floorp.gpg
-sudo curl -sS --compressed -o /etc/apt/sources.list.d/Floorp.list 'https://ppa.ablaze.one/Floorp.list'
-apt update
-apt install floorp
+    neofetch \
+    nvidia-driver firmware-misc-nonfree \
+curl -fsSL https://ppa.ablaze.one/KEY.gpg | sudo gpg --dearmor -o /usr/share/keyrings/Floorp.gpg \
+sudo curl -sS --compressed -o /etc/apt/sources.list.d/Floorp.list 'https://ppa.ablaze.one/Floorp.list' \
+apt update \
+apt install floorp \
 
 echo 
 echo xfce install complete, please reboot and issue 'startx'
